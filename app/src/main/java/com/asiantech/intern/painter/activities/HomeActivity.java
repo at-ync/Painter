af515ba6b1c93331ec791b19ce4ba269e7f647f2
at-ync <ync@asiantech.vn>
2016-08-30 15:54:13 +0700

    create ui home screen

---
+++ app/src/main/java/com/asiantech/intern/painter/activities/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.asiantech.intern.painter.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.asiantech.intern.painter.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_home)
+public class HomeActivity extends AppCompatActivity {
+
+}
