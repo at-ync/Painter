0181dfd31865a36bfd0e0c2a815d6b4685cfbfe6
at-ync <ync@asiantech.vn>
2016-09-06 22:17:06 +0700

    New changelist

---
+++ app/src/main/java/com/asiantech/intern/painter/utils/IClickItemRecyclerView.java
@@ -0,0 +1,12 @@
+package com.asiantech.intern.painter.utils;
+
+import android.view.View;
+
+/**
+ * Copyright © 2016 YNC.
+ * Created by Congybk on 6/14/2016.
+ */
+ public interface IClickItemRecyclerView {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
